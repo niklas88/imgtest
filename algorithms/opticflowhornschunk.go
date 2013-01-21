8473b6086001a8089aa1e68c01bffe49c0e7bb8d
Niklas Schnelle <niklas@komani.de>
2013-01-21 20:52:24 +0100

    Use Goroutines (one per image line as first try)

--- algorithms/opticflowhornschunk.go
+++ algorithms/opticflowhornschunk.go
@@ -7,6 +7,7 @@ package algorithms
 import (
 	"github.com/niklas88/imgtest/floatimage"
 	"math"
+	"sync"
 )
 
 // Derivatives fx, fy, fz as 3 channel FloatImg to make access sane
@@ -35,53 +36,61 @@ func deriveMixed(f1, f2 *floatimage.FloatImg) *floatimage.FloatImg {
 
 func flow(alpha float32, derivs, oldvec, vecField *floatimage.FloatImg) {
 	bounds := vecField.Bounds()
-
-	help := 1.0 / alpha
-	var nn int
-	var uSum, vSum float32
-	var uv []float32
+	count := bounds.Max.Y - bounds.Min.Y
+	var wg sync.WaitGroup
+	wg.Add(count)
 	for j := bounds.Min.Y; j < bounds.Max.Y; j++ {
-		for i := bounds.Min.X; i < bounds.Max.X; i++ {
-			nn = 0
-			uSum, vSum = 0, 0
-			if i > bounds.Min.X {
-				nn++
-				uv = oldvec.AtF(i-1, j)
-				uSum += uv[0]
-				vSum += uv[1]
-			}
+		go func(j int) {
+			bounds := vecField.Bounds()
+			help := 1.0 / alpha
+			var nn int
+			var uSum, vSum float32
+			var uv []float32
 
-			if i < bounds.Max.X-1 {
-				nn++
-				uv = oldvec.AtF(i+1, j)
-				uSum += uv[0]
-				vSum += uv[1]
-			}
+			for i := bounds.Min.X; i < bounds.Max.X; i++ {
+				nn = 0
+				uSum, vSum = 0, 0
+				if i > bounds.Min.X {
+					nn++
+					uv = oldvec.AtF(i-1, j)
+					uSum += uv[0]
+					vSum += uv[1]
+				}
 
-			if j > bounds.Min.Y {
-				nn++
-				uv = oldvec.AtF(i, j-1)
-				uSum += uv[0]
-				vSum += uv[1]
-			}
+				if i < bounds.Max.X-1 {
+					nn++
+					uv = oldvec.AtF(i+1, j)
+					uSum += uv[0]
+					vSum += uv[1]
+				}
 
-			if j < bounds.Max.Y-1 {
-				nn++
-				uv = oldvec.AtF(i, j+1)
-				uSum += uv[0]
-				vSum += uv[1]
+				if j > bounds.Min.Y {
+					nn++
+					uv = oldvec.AtF(i, j-1)
+					uSum += uv[0]
+					vSum += uv[1]
+				}
+
+				if j < bounds.Max.Y-1 {
+					nn++
+					uv = oldvec.AtF(i, j+1)
+					uSum += uv[0]
+					vSum += uv[1]
+				}
+				dvs := derivs.AtF(i, j)
+				fxij, fyij, fzij := dvs[Fxc], dvs[Fyc], dvs[Fzc]
+				uv = oldvec.AtF(i, j)
+				uSum -= help * fxij * (fyij*uv[1] + fzij)
+				uSum /= float32(nn) + help*fxij*fxij
+				vSum -= help * fyij * (fxij*uv[0] + fzij)
+				vSum /= float32(nn) + help*fyij*fyij
+				uv = vecField.AtF(i, j)
+				uv[0], uv[1] = uSum, vSum
 			}
-			dvs := derivs.AtF(i, j)
-			fxij, fyij, fzij := dvs[Fxc], dvs[Fyc], dvs[Fzc]
-			uv = oldvec.AtF(i, j)
-			uSum -= help * fxij * (fyij*uv[1] + fzij)
-			uSum /= float32(nn) + help*fxij*fxij
-			vSum -= help * fyij * (fxij*uv[0] + fzij)
-			vSum /= float32(nn) + help*fyij*fyij
-			uv = vecField.AtF(i, j)
-			uv[0], uv[1] = uSum, vSum
-		}
+			wg.Done()
+		}(j)
 	}
+	wg.Wait()
 }
 
 // OpticFlowHornSchunk computes the optic flow between two images
